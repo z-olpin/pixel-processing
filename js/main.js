ca24467860927de4257dbe832c65d654c9fb3135
Zachary Olpin <info@zacharyolp.in>
2020-08-26 14:16:52 -0600

    fix img load event not firing if image loads before script

--- js/main.js
+++ js/main.js
@@ -68,15 +68,16 @@ const uploadHandler = () => {
 }
 
 const draw = () => {
-  URL.revokeObjectURL(img.src)
   if (img.width > 1000) img.width = 1000
   canvas.height = img.height;
   canvas.width = img.width;
   img.style.maxWidth = `${img.width}px`
   canvas.style.maxWidth = `${img.width}px`
   ctx.drawImage(img, 0, 0, img.width, img.height)
+  URL.revokeObjectURL(img.src)
 }
 
+if (img.complete) draw()
 img.addEventListener('load', draw)
 upload.addEventListener('change', uploadHandler)
 invertEm.addEventListener('click', invert)
